6c605a10b5275e7493c1237cd73f438721bd19f7
ROBOJessie2019 <57692031+R080Jessica20@users.noreply.github.com>
2022-01-31 10:40:54 -0600

    Update PlayervsPlayer.java

--- Tic_Tac_Toe/src/PlayervsPlayer.java
+++ Tic_Tac_Toe/src/PlayervsPlayer.java
@@ -27,10 +27,11 @@ public class PlayervsPlayer{
         System.out.println(gamepull.getPlayers());
         gamepull.setSTATUS(1);
         XorO();
+        gamepull.fillBoard();
         while (gamepull.getSTATUS() == gamepull.PLAYING) {
             if( playerTurn==true && player2Turn==false){
                 gamepull.displayBoard(gamepull.board);
-               // System.out.println(gamepull.getplayerNames(0));
+            //   System.out.println(gamepull.getplayerNames(0));
                 System.out.println("Enter a column from 0 to 3: ");
                 col = in.nextInt();
                 System.out.println("Enter a row from 0 to 3: ");
@@ -38,7 +39,7 @@ public class PlayervsPlayer{
                 System.out.println("Enter the board from 0 to 3");
                 sheet = in.nextInt();
                 Location area = new Location(col, row, sheet);
-                if (gamepull.board[col][row][sheet] == 0) {
+                if (gamepull.board[col][row][sheet] == '-') {
 
                     if (player == 'x') {
                         gamepull.setBoard(col,row,sheet,'x');
@@ -64,7 +65,7 @@ public class PlayervsPlayer{
                 System.out.println("Enter the board from 0 to 3");
                 sheet = in.nextInt();
                 Location area = new Location(col, row, sheet);
-                if (gamepull.board[col][row][sheet] == 0) {
+                if (gamepull.board[col][row][sheet] == '-') {
 
                     if (player2 == 'x') {
                         gamepull.setBoard(col,row,sheet,'x');
@@ -81,7 +82,10 @@ public class PlayervsPlayer{
                     playerTurn = true;
                 }
             }
+            gamepull.isWinner(gamepull.board);
+            gamepull.count(gamepull.board);
         }
         }
 }
+
 
