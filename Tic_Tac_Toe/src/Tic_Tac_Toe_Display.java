5b328cb48d90c6b5ed0a9e4446737e42c104856f
zoepuno <98069599+zoepuno@users.noreply.github.com>
2022-02-07 15:06:22 -0600

    Update Tic_Tac_Toe_Display.java

--- Tic_Tac_Toe/src/Tic_Tac_Toe_Display.java
+++ Tic_Tac_Toe/src/Tic_Tac_Toe_Display.java
@@ -1,14 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.RoundRectangle2D;
 import java.util.ArrayList;
+import java.util.Scanner;
 //Don't push to master make new repository for Elevator
 //https://docs.oracle.com/javase/tutorial/uiswing/events/keylistener.html
 //https://stackoverflow.com/questions/16728591/add-the-characters-of-word-into-the-jbuttons
-public class Tic_Tac_Toe_Display extends JPanel implements MouseListener {
+
+
+public class Tic_Tac_Toe_Display extends JPanel implements MouseListener,Runnable{
     static ArrayList<RoundRectangle2D.Double> rec = new ArrayList<RoundRectangle2D.Double>();
     static ArrayList<RoundRectangle2D.Double> choice1 = new ArrayList<RoundRectangle2D.Double>();
     static ArrayList<RoundRectangle2D.Double> choice2 = new ArrayList<RoundRectangle2D.Double>();
@@ -17,58 +19,71 @@ public class Tic_Tac_Toe_Display extends JPanel implements MouseListener {
     boolean SceneOne;
     boolean SceneTwo;
     boolean SceneThree;
-    boolean GameStarted;
+    private Thread t;
+
     boolean playervsplayer;
     boolean playervsAI;
     boolean playervsRandomAi;
     int m;
-    static Game maingame = new Game();
+    static Game mg = new Game();
     Main startGame = new Main();
+
     public Tic_Tac_Toe_Display() {
+
+        // Sets it's size
+        setSize(800, 1200);
+        setVisible(true);
+
+        addMouseListener(this);
         rec= new ArrayList<>();
         SceneOne=true;
-        addMouseListener(this);
-        setSize(800, 1200);
-        rec.add(new RoundRectangle2D.Double(250.0, 530.0, 240.0, 100.0, 0, 0));
-        choice1.add(new RoundRectangle2D.Double(170, 460, 400,50,0,0));
-        choice2.add(new RoundRectangle2D.Double(170, 560, 460,50,0,0));
-        choice3.add(new RoundRectangle2D.Double(170, 660, 520,50,0,0));
-        RandomAi.add(new RoundRectangle2D.Double(170, 460, 400,50,0,0));
+        rec.add(new RoundRectangle2D.Double(250.0, 530.0, 240.0, 100.0, 10, 10));
+        choice1.add(new RoundRectangle2D.Double(170, 460, 400,50,10,10));
+        choice2.add(new RoundRectangle2D.Double(170, 560, 460,50,10,10));
+        choice3.add(new RoundRectangle2D.Double(170, 660, 520,50,10,10));
+        RandomAi.add(new RoundRectangle2D.Double(170, 460, 400,50,10,10));
+        repaint();
+
     }
 
-    public static void main(String args[]) {
-        JFrame frame = new JFrame("Tic Tac Toe");
-    }
     public void paint(Graphics g) {
+        addNotify();
         Graphics2D p = (Graphics2D) g;
-        p.setPaint(new Color(36,37,38));
-        p.fillRect(0, 0, 800, 1200);
-        if(SceneOne==true) {
-    // R 36,  G 37,  B 38
-    p.setPaint(new Color(255, 128, 128));
-    p.setFont(new Font("Sans Serif", Font.BOLD, 60));
-    p.drawString("Welcome To", 206, 200);
-    p.drawString("Tic Tac Toe", 206, 300);
 
-    p.setPaint(Color.RED);
-    p.setFont(new Font("Sans Serif", Font.BOLD, 60));
-    p.drawString("Welcome To", 200, 200);
-    p.drawString("Tic Tac Toe", 200, 300);
-    p.setPaint(new Color(36,37,38));
+        p.setPaint(new Color(36, 37, 38));
+        p.fillRect(0, 0, 800, 1200);
+
+        //intro to play
+        if (SceneOne == true) {
+            // R 36,  G 37,  B 38
+            p.setPaint(new Color(255, 128, 128));
+            p.setFont(new Font("Sans Serif", Font.BOLD, 60));
+            p.drawString("Welcome To", 206, 200);
+            p.drawString("Tic Tac Toe", 206, 300);
+
+            p.setPaint(Color.RED);
+            p.setFont(new Font("Sans Serif", Font.BOLD, 60));
+            p.drawString("Welcome To", 200, 200);
+            p.drawString("Tic Tac Toe", 200, 300);
+
+
+            p.setPaint(new Color(36, 37, 38));
             for (int i = 0; i < rec.size(); i++) {
                 RoundRectangle2D.Double rectangle = rec.get(i);
                 ((Graphics2D) g).fill(rectangle);
             }
-    p.setPaint(new Color(255, 128, 128));
-    p.drawString("PLAY?", 276, 600);
-    p.setFont(new Font("Sans Serif", Font.BOLD, 60));
-    p.setPaint(new Color(255, 128, 128));
-    p.drawRect(251, 530, 240, 100);
-    p.setPaint(Color.RED);
-    p.drawString("PLAY?", 270, 600);
-    p.drawRect(250, 530, 240, 100);
+            p.setPaint(new Color(255, 128, 128));
+            p.drawString("PLAY?", 276, 600);
+            p.setFont(new Font("Sans Serif", Font.BOLD, 60));
+            p.setPaint(new Color(163, 185, 224));
+            p.drawRect(251, 530, 240, 100);
+            p.setPaint(Color.RED);
+            p.drawString("PLAY?", 270, 600);
+            p.drawRect(250, 530, 240, 100);
         }
-      else  if(SceneTwo==true) {
+
+        //choose gamemode
+        else if (SceneTwo == true) {
             p.setPaint(new Color(255, 128, 128));
             p.setFont(new Font("Sans Serif", Font.BOLD, 60));
             p.drawString("Choose your", 200, 200);
@@ -80,7 +95,7 @@ public class Tic_Tac_Toe_Display extends JPanel implements MouseListener {
             p.drawString("Choose your", 206, 200);
             p.setPaint(Color.BLUE);
             p.drawString("Gamemode:", 220, 300);
-            p.setPaint(new Color(36,37,38));
+            p.setPaint(new Color(36, 37, 38));
             //rec choice
             for (int i = 0; i < rec.size(); i++) {
                 RoundRectangle2D.Double rectangle = choice1.get(i);
@@ -96,108 +111,129 @@ public class Tic_Tac_Toe_Display extends JPanel implements MouseListener {
             }
             p.setPaint(new Color(255, 128, 128));
             p.setFont(new Font("Sans Serif", Font.BOLD, 40));
-            p.drawString("1. Player vs Player ",186, 500);
+            p.drawString("1. Player vs Player ", 186, 500);
             p.setPaint(new Color(128, 140, 255));
-            p.drawString("2. Player vs Computer ",186, 600);
+            p.drawString("2. Player vs Computer ", 186, 600);
             p.setPaint(new Color(255, 128, 128));
-            p.drawString("3. Computer vs Computer ",186, 700);
+            p.drawString("3. Computer vs Computer ", 186, 700);
             p.setPaint(Color.RED);
             p.setFont(new Font("Sans Serif", Font.BOLD, 40));
-            p.drawRect(170, 460, 400,50);
-            p.drawString("1. Player vs Player ",190, 500);
+            p.drawRect(170, 460, 400, 50);
+            p.drawString("1. Player vs Player ", 190, 500);
             p.setPaint(Color.BLUE);
-            p.drawRect(170, 560, 460,50);
-            p.drawString("2. Player vs Computer ",190, 600);
+            p.drawRect(170, 560, 460, 50);
+            p.drawString("2. Player vs Computer ", 190, 600);
             p.setPaint(Color.RED);
-            p.drawRect(170, 660, 520,50);
-            p.drawString("3. Computer vs Computer ",190, 700);
-
-
+            p.drawRect(170, 660, 520, 50);
+            p.drawString("3. Computer vs Computer ", 190, 700);
         }
-       else if(SceneThree==true) {
-           if(playervsplayer){
 
-           }
-if(playervsAI==true){
-    p.setPaint(new Color(255, 128, 128));
-    p.setFont(new Font("Sans Serif", Font.BOLD, 60));
-    p.drawString("Choose your", 200, 200);
-    //128, 140, 255
-    p.setPaint(new Color(128, 140, 255));
-    p.drawString("AI:", 347, 300);
-    p.setFont(new Font("Sans Serif", Font.BOLD, 60));
-    p.setPaint(Color.RED);
-    p.drawString("Choose your", 206, 200);
-    p.setPaint(Color.BLUE);
-    p.drawString("AI:", 350, 300);
-    //choices
-    p.setPaint(new Color(36,37,38));
-    for (int i = 0; i < RandomAi.size(); i++) {
-        RoundRectangle2D.Double rectangle = RandomAi.get(i);
-        ((Graphics2D) g).fill(rectangle);
-    }
-    p.setPaint(new Color(255, 128, 128));
-    p.setFont(new Font("Sans Serif", Font.BOLD, 40));
-    p.drawString("1. Random AI ",186, 500);
-    p.setPaint(Color.RED);
-    p.drawString("1. Random AI ",190, 500);
-    p.drawRect(170, 460, 400,50);
-}
-if(GameStarted==true){
-    p.setPaint(Color.WHITE);
-    int[] a = {100, 200, 225, 125, 100};
-    int[] b = {550, 550, 525, 525, 550};
+        //game type begins
+        else if (SceneThree == true) {
+            if (playervsplayer) {
 
-    for (int s = 0; s < 4; s++) {
-        for (int r = 0; r < 4; r++) {
-            for (int c = 0; c < 4; c++) {
-                p.drawPolygon(a, b, 5);
-                for (m = 0; m < 5; m++)
-                    a[m] += 100;
             }
-            for (m = 0; m < 5; m++) {
-                a[m] -= 375;
-                b[m] -= 25;
+            if (playervsAI == true) {
+                p.setPaint(new Color(255, 128, 128));
+                p.setFont(new Font("Sans Serif", Font.BOLD, 60));
+                p.drawString("Choose your", 200, 200);
+                p.setPaint(new Color(128, 140, 255));
+                p.drawString("AI:", 347, 300);
+                p.setFont(new Font("Sans Serif", Font.BOLD, 60));
+                p.setPaint(Color.RED);
+                p.drawString("Choose your", 206, 200);
+                p.setPaint(Color.BLUE);
+                p.drawString("AI:", 350, 300);
+                //choices
+                p.setPaint(new Color(36, 37, 38));
+                for (int i = 0; i < RandomAi.size(); i++) {
+                    RoundRectangle2D.Double rectangle = RandomAi.get(i);
+                    ((Graphics2D) g).fill(rectangle);
+                }
+                p.setPaint(new Color(255, 128, 128));
+                p.setFont(new Font("Sans Serif", Font.BOLD, 40));
+                p.drawString("1. Random AI ", 186, 500);
+                p.setPaint(Color.RED);
+                p.drawString("1. Random AI ", 190, 500);
+                p.drawRect(170, 460, 400, 50);
             }
         }
-        for (m = 0; m < 5; m++) {
-            a[m] -= 100;
-            b[m] -= 25;
-        }
-    }
-    //prints player turn
+
+        if (mg.STATUS == mg.PLAYING) {
+            //Prints Board
+            int[] a = {100, 200, 225, 125, 100};
+            int[] b = {550, 550, 525, 525, 550};
+            Location area = new Location(0, 0, 0);
+            for (int s = 0; s < 4; s++) {
+                for (int r = 0; r < 4; r++) {
+                    for (int c = 0; c < 4; c++) {
+                        p.setPaint(Color.WHITE);
+                        p.drawPolygon(a, b, 5);
+
+                        area.setSheet(s);
+                        area.setRow(r);
+                        area.setCol(c);
+                        char type = Main.mg.board.getPoint(area);
+
+                        if (type == 'x') {
+                            p.setPaint(new Color(255, 128, 128));
+                            p.fillPolygon(a, b, 5);
+                        }
+
+                        if (type == 'o') {
+                            p.setPaint(new Color(128, 140, 255));
+                            p.fillPolygon(a, b, 5);
+                        }
+                        for (m = 0; m < 5; m++)
+                            a[m] += 100;
+                    }
+                    for (m = 0; m < 5; m++) {
+                        a[m] -= 375;
+                        b[m] -= 25;
+                    }
+                }
+                for (m = 0; m < 5; m++) {
+                    a[m] -= 100;
+                    b[m] -= 25;
+                }
+            }
+
+            //prints player turn
             p.setPaint(new Color(226, 225, 187));
             p.setFont(new Font("Sans Serif", Font.BOLD, 30));
             p.drawString("Player Turn: ", 100, 600);
-            if (mg.playerturn == 1)//p1
-            {
+            //p1
+            if (Main.mg.playerturn == 1) {
                 p.setPaint(new Color(234, 153, 150));
                 p.setFont(new Font("Sans Serif", Font.BOLD, 30));
                 p.drawString(Main.mg.player1.getName(), 300, 600);
             }
             //p2
-            if (mg.playerturn == 2) {
+            if (Main.mg.playerturn == 2) {
                 p.setPaint(new Color(163, 185, 224));
                 p.setFont(new Font("Sans Serif", Font.BOLD, 30));
                 p.drawString(Main.mg.player2.getName(), 300, 600);
             }
-}
-if(playervsrandomAI==true){
-    //Put Code Here!
-      //sets up person as first player
-            mg.player2.name = "player";
+            play();
+        }
+    }
+    public void play(){
 
-            //sets up AI as second player
-            Random_Ai random = new Random_Ai("Random", 'x');
-            Main.mg.player2.name = random.getName();
+            if (playervsRandomAi == true) {
+                //sets up person as first player
+                mg.player2.name = "player";
+                
+                //sets up AI as second player
+                Random_Ai random = new Random_Ai("Random", 'x');
+                mg.player2.name = random.getName();
 
-            System.out.println("Hello" + Main.mg.player1.name);
-            System.out.println("Player Two is " + random.getName());
-     do {
+                System.out.println("Hello" + mg.player1.name);
+                System.out.println("Player Two is " + random.getName());
+
                 // Let player 1 take a turn
-                if (Main.mg.playerturn == 1) {
-                    if (Main.mg.moveCheck(Main.mg.board.board, Main.mg.p1)) {  // make sure it's a valid move
-                        Main.mg.board.setBoard(Main.mg.p1, Main.mg.player1.getLetter());
+                if (mg.playerturn == 1) {
+                    if (mg.moveCheck(Main.mg.board.board, mg.p1)) {  // make sure it's a valid move
+                        mg.board.setBoard(mg.p1, Main.mg.player1.getLetter());
 
                         if (Main.mg.board.count()) {
                             System.out.println("Game ends in a draw.");
@@ -208,79 +244,92 @@ if(playervsrandomAI==true){
                         }
                     }
                 }
+                //p2
                 // Let the Ai take a turn
                 if (Main.mg.playerturn == 2 && !Main.mg.gameOver) {
                     random.random_move(Main.mg.board.board);
                     Main.mg.p2 = new Location(random.getC(), random.getR(), random.getS());
                     if (Main.mg.moveCheck(Main.mg.board.board, Main.mg.p2))  // make sure it's valid move
                         Main.mg.board.setBoard(Main.mg.p2, Main.mg.player2.letter);
+
+                    if (Main.mg.board.count()) {
+                        System.out.println("Game ends in a draw.");
+                        Main.mg.gameOver = true;
+                    } else if (Main.mg.isWinner(Main.mg.board.board) == Main.mg.PlAYER2) {
+                        System.out.println("P2 wins!");
+                        Main.mg.gameOver = true;
+                    }
                 }
                 Main.mg.setPlayerturn();
-            } while (!mg.gameOver);
-            if (Main.mg.board.count()) {
-                System.out.println("Game ends in a draw.");
-                Main.mg.gameOver = true;
             }
-            else if (Main.mg.isWinner(Main.mg.board.board) == Main.mg.PlAYER2) {
-                System.out.println("P2 wins!");
-                Main.mg.gameOver = true;
-            }
-    
- 
-}
+        }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         repaint();
     }
-
     @Override
-    public void mouseClicked(MouseEvent e) {
+    public void mouseClicked (MouseEvent e) {
         e.getButton();
         for (int i = 0; i < rec.size(); i++) {
             RoundRectangle2D.Double rectangle = rec.get(i);
             if (rectangle.contains(e.getPoint())) {
-           SceneTwo=true;
-           SceneOne=false;
-           SceneThree=false;
-                startGame.playing=1;
-                }
+                SceneTwo = true;
+                SceneOne = false;
+                SceneThree = false;
             }
+        }
         for (int i = 0; i < choice1.size(); i++) {
             RoundRectangle2D.Double rectangle = choice1.get(i);
             if (rectangle.contains(e.getPoint())) {
                 SceneTwo = false;
                 SceneThree = true;
                 playervsplayer = true;
-                GameStarted=true;
-                startGame.choice=1;
             }
         }
-            for (int i = 0; i < choice2.size(); i++) {
-                RoundRectangle2D.Double rectangle = choice2.get(i);
-                if (rectangle.contains(e.getPoint())) {
-                    SceneTwo = false;
-                    SceneThree = true;
-                    playervsAI=true;
-                    startGame.choice=2;
-                }
+        for (int i = 0; i < choice2.size(); i++) {
+
+            RoundRectangle2D.Double rectangle = choice2.get(i);
+            if (rectangle.contains(e.getPoint())) {
+                SceneTwo = false;
+                SceneThree = true;
+                playervsAI = true;
+            }
         }
+
         for (int i = 0; i < RandomAi.size(); i++) {
+
             RoundRectangle2D.Double rectangle = RandomAi.get(i);
             if (rectangle.contains(e.getPoint())) {
-                playervsAI=false;
-                playervsRandomAi=true;
-                GameStarted=true;
-                startGame.randomAI=1;
+                playervsAI = false;
+                playervsRandomAi = true;
+                mg.STATUS = mg.PLAYING;
             }
+
+            //click on grid
+            if (mg.STATUS == mg.PLAYING) {
+                int x = e.getX(), y = e.getY();
+                System.out.println(x + "," + y);
+                if (Main.mg.playerturn == 1)
+                    Main.mg.p1 = Location(x, y);
+                if (Main.mg.playerturn == 2)
+                    Main.mg.p2 = Location(x, y);
+
+                if (Main.mg.playerturn == 1)//player 1 spot
+                    Main.mg.p1 = Location(x, y);
+
+                if (Main.mg.gameOver)
+                    System.out.println("Press REPLAY button to play again.");
+                repaint();
+            }
+
         }
-
-           //click on grid
-            int x = e.getX(), y = e.getY();
-            System.out.println(x+","+y);
-            Location(x,y);
-
-            repaint();
-        }
-
+    }
     public Location Location(int x, int y)
     {
         int c = 0;
@@ -409,6 +458,7 @@ if(playervsrandomAI==true){
         repaint();
     }
 }
+
 
 
 
