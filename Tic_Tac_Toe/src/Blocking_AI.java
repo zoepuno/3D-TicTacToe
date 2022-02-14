5fbfff3ff7280ae623bbd59239a659b33c1d4801
ROBOJessie2019 <57692031+R080Jessica20@users.noreply.github.com>
2022-02-14 11:12:26 -0600

    Update Blocking_AI.java

--- Tic_Tac_Toe/src/Blocking_AI.java
+++ Tic_Tac_Toe/src/Blocking_AI.java
@@ -1,13 +1,14 @@
 public class Blocking_AI implements PlayerInt {
     char[][][] boardCopy = new char[4][4][4];
     Player play = new Player(null, '0');
-    Game game = new Game();
     int col = 0;
     int row = 0;
     boolean done;
     Location boo;
+    char board[][][];
+    boolean moved;
 
-    public Blocking_AI() {
+    public Blocking_AI( ) {
 
     }
 
@@ -65,17 +66,18 @@ public class Blocking_AI implements PlayerInt {
             for (int l = 0; l < 4; l++) {
                 for (int c = 0; c < 4; c++) {
                     for (int r = 0; r < 4; r++) {
-                        if (game.board.board[l][c][r] == 'x') {
+                        if (board[l][c][r] == 'x') {
                             boardCopy[l][c][r] = 'x';
                         }
                     }
                 }
             }
-        } else {
+        }
+        if (Tic_Tac_Toe_Display.Ai2 == false) {
             for (int l = 0; l < 4; l++) {
                 for (int c = 0; c < 4; c++) {
                     for (int r = 0; r < 4; r++) {
-                        if (game.board.board[c][r][l] == 'o') {
+                        if (board[c][r][l] == 'o') {
                             boardCopy[c][r][l] = 'o';
                         }
                     }
@@ -84,75 +86,79 @@ public class Blocking_AI implements PlayerInt {
         }
     }
 
-    public void move(char player) {
-        for (int l = 0; l < 4; l++) {
-            for (int c = 0; c < 4; c++) {
-                for (int r = 0; r < 4; r++) {
-                    //col
-                    if (!(boardCopy[c][0][l] == player)) {
-                        col++;
-                        if (col == 3) {
-                            for (int v = 0; c < 4; c++) {
-                                if (game.board.board[v][0][l] == '-') {
-                                    boo = new Location(v, 0, l);
-                                    break;
-                                }
-                            }
-                        }
-                        if (col == 2) {
-                            for (int v = 0; c < 4; c++) {
-                                if (game.board.board[v][0][l] == '-') {
-                                    boo = new Location(v, 0, l);
-                                    break;
-                                }
-                            }
-                        }
-                    } else if (!(boardCopy[c][0][l] == player)) {
-                        col++;
-                        if (col == 3) {
-                            for (int v = 0; c < 4; c++) {
-                                if (game.board.board[v][0][l] == '-') {
+    public void move(char player, char board[][][], boolean moved) {
+        while(moved==false) {
 
-                                    boo = new Location(v, 0, l);
-                                    break;
+            for (int l = 0; l < 4; l++) {
+                for (int c = 0; c < 4; c++) {
+                    for (int r = 0; r < 4; r++) {
+                        //col
+                        if (!(boardCopy[c][0][l] == player)) {
+                            col++;
+                            if (col == 3) {
+                                for (int v = 0; c < 4; c++) {
+                                    if (board[v][0][l] == '-') {
+                                        boo = new Location(v, 0, l);
+                                        break;
+                                    }
                                 }
                             }
-                        }
-                        if (col == 2) {
-                            for (int v = 0; c < 4; c++) {
-                                if (game.board.board[v][0][l] == '-') {
-
-                                    boo = new Location(v, 0, l);
-                                    break;
+                            if (col == 2) {
+                                for (int v = 0; c < 4; c++) {
+                                    if (board[v][0][l] == '-') {
+                                        boo = new Location(v, 0, l);
+                                        break;
+                                    }
                                 }
                             }
-                        }
-                    } else {
-                        //corner first
-                        if (game.board.board[0][0][l] == '-') {
-                            boo = new Location(c, r, l);
-                            break;
-                        } else if (game.board.board[3][3][l] == '-') {
-                            boo = new Location(c, r, l);
-                            break;
-                        } else if (game.board.board[0][3][l] == '-') {
-                            boo = new Location(c, r, l);
-                            break;
-                        } else if (game.board.board[3][0][l] == '-') {
-                            boo = new Location(c, r, l);
-                            break;
+                        } else if (!(boardCopy[c][0][l] == player)) {
+                            col++;
+                            if (col == 3) {
+                                for (int v = 0; c < 4; c++) {
+                                    if (board[v][0][l] == '-') {
 
+                                        boo = new Location(v, 0, l);
+                                        break;
+                                    }
+                                }
+                            }
+                            if (col == 2) {
+                                for (int v = 0; c < 4; c++) {
+                                    if (board[v][0][l] == '-') {
+
+                                        boo = new Location(v, 0, l);
+                                        break;
+                                    }
+                                }
+                            }
+                        } else {
+                            //corner first
+                            if (board[0][0][l] == '-') {
+                                boo = new Location(c, r, l);
+                                break;
+                            } else if (board[3][3][l] == '-') {
+                                boo = new Location(c, r, l);
+                                break;
+                            } else if (board[0][3][l] == '-') {
+                                boo = new Location(c, r, l);
+                                break;
+                            } else if (board[3][0][l] == '-') {
+                                boo = new Location(c, r, l);
+                                break;
+
+                            }
                         }
+
                     }
-
                 }
             }
-        }
-            game.board.board[boo.getSheet()][boo.getRow()][boo.getCol()] = player;
-            System.out.print("Coolio");
+            board[boo.getSheet()][boo.getRow()][boo.getCol()] = player;
+            System.out.print(board[boo.getSheet()][boo.getRow()][boo.getCol()]);
+            System.out.print("\n");
             changeBoard();
             displayBoard(boardCopy);
-            Tic_Tac_Toe_Display.moved=true;
+            moved = true;
+        }
 
 
     }
