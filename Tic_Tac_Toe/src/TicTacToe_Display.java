ba556e87e1b9590112f772066c59ab17dc4c5e7d
zoepuno <98069599+zoepuno@users.noreply.github.com>
2022-02-14 11:09:14 -0600

    Update TicTacToe_Display.java

--- Tic_Tac_Toe/src/TicTacToe_Display.java
+++ Tic_Tac_Toe/src/TicTacToe_Display.java
@@ -4,17 +4,17 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.Random;
 import java.util.Scanner;
 
-public class Tic_Tac_Toe_Display extends JPanel implements MouseListener, Runnable, KeyListener {
+public class TicTacToe_Display extends JPanel implements MouseListener, Runnable, KeyListener {
 
     Polygon[][][] tile = new Polygon[4][4][4];
     char[][][] board = new char[4][4][4];
     char[][][] winBoard = new char[4][4][4];
     char p1 = 'x', p2 = 'o';
     boolean playerTurn = true;
-    static boolean Ai1 = false, Ai2 = false;
+    boolean Ai1 = false, Ai2 = false;
+    boolean random, blocking, moved;
 
     int GamePause = 0;
     int millisToSleep = 20;
@@ -24,65 +24,57 @@ public class Tic_Tac_Toe_Display extends JPanel implements MouseListener, Runnab
     int p2Wins = 0;
     int i;
     boolean replay = false;
-    Blocking_AI block= new Blocking_AI();
-    static boolean moved;
-    boolean random;
-    boolean blocking;
-    Board boards = new Board();
 
-    public Tic_Tac_Toe_Display() {
+    public TicTacToe_Display() {
         super();
         addMouseListener(this);
         addKeyListener(this);
-        setSize(800,1200);
-        Scanner sc = new Scanner(System.in);
+        setSize(800, 1200);
 
-        for(int c=0;c<4;c++){
+        for (int c = 0; c < 4; c++) {
             int align = 100;
-            for(int s=0;s<4;s++){
-                for(int r=0;r<4;r++){
-                    int xPoints[] = {align+10+50*r, align+60+50*r, align+50+50*r,align+50*r};
-                    int yPoints[] = {60+40*s+200*c, 60+40*s+200*c, 100+40*s+200*c,100+40*s+200*c};
-                    tile[r][c][3-s] = new Polygon(xPoints,yPoints, 4);
+            for (int s = 0; s < 4; s++) {
+                for (int r = 0; r < 4; r++) {
+                    int xPoints[] = {align + 10 + 50 * r, align + 60 + 50 * r, align + 50 + 50 * r, align + 50 * r};
+                    int yPoints[] = {60 + 40 * s + 200 * c, 60 + 40 * s + 200 * c, 100 + 40 * s + 200 * c, 100 + 40 * s + 200 * c};
+                    tile[r][c][3 - s] = new Polygon(xPoints, yPoints, 4);
 
                     board[r][c][s] = '-';
                     winBoard[r][c][s] = '-';
                 }
-                align-=10;
+                align -= 10;
             }
         }
 
 
         System.out.print("Do you want player x to be AI? (type 'y' or 'n'): ");
-        if(getStringput().charAt(0) =='y') {
+        if (getStringput().charAt(0) == 'y') {
             Ai1 = true;
-            System.out.print("Random AI or Blocking AI: ");
-            if(getStringput().charAt(0) =='r') {
-                random=true;
+            System.out.print("Random AI(r) or Blocking AI(b): ");
+            if (getStringput().charAt(0) == 'r') {
+                random = true;
             }
-            else{
-                block.setBoardCopy();
-                blocking=true;
-            }
-            }
-
-        System.out.print("Do you want player o to be AI? (type 'y' or 'n'): ");
-        if(getStringput().charAt(0) =='y') {
-            Ai2 = true;
-            System.out.print("Random AI or Blocking AI: ");{
-                if(getStringput().charAt(0) =='r') {
-                    random=true;
-                }
-                else{
-                    block.setBoardCopy();
-                    blocking=true;
-                }
+            if (getStringput().charAt(0) == 'b') {
+                blocking = true;
             }
         }
+
+        System.out.print("Do you want player o to be AI? (type 'y' or 'n'): ");
+        if (getStringput().charAt(0) == 'y'){
+            Ai2 = true;
+        System.out.print("Random AI(r) or Blocking AI(b): ");
+        if (getStringput().charAt(0) == 'r') {
+            random = true;
+        }
+        if (getStringput().charAt(0) == 'b') {
+            blocking = true;
+        }
+    }
+
         if(Ai1 && Ai2){
             System.out.print("How many milliseconds between AI moves?: ");
             millisToSleep = getIntput();
-            System.out.print("How many games you want the AI to play?: ");
+            System.out.print("How many games you want the AIs to play?: ");
             nGames = getIntput();
             System.out.print("How many milliseconds between games?: ");
             GamePause = getIntput();
@@ -97,9 +89,6 @@ public class Tic_Tac_Toe_Display extends JPanel implements MouseListener, Runnab
 
         if(!Ai1 || !Ai2) {
             if (i == nGames) {
-                System.out.println("i: "+i);
-                System.out.println("nGames: "+nGames);
-                System.out.println("new turn");
             }
             for(int r=0;r<4;r++){
                 for(int c=0;c<4;c++){
@@ -127,25 +116,8 @@ public class Tic_Tac_Toe_Display extends JPanel implements MouseListener, Runnab
 
     }
 
-
-    public static int getIntput(){
-        Scanner in = new Scanner(System.in);
-        while(true){
-            if(in.hasNextInt())
-                return in.nextInt();
-            in.next();
-        }
-    }
-
-    public static String getStringput(){
-        Scanner in = new Scanner(System.in);
-        while(true){
-            if(in.hasNext())
-                return in.next();
-        }
-    }
-
     public void paint(Graphics p){
+
         p.setColor(Color.BLACK);
         p.fillRect(0,0,800,1200);
         p.setColor(new Color(255, 128, 128));
@@ -153,12 +125,12 @@ public class Tic_Tac_Toe_Display extends JPanel implements MouseListener, Runnab
 
         for(int c=0;c<4;c++){
             for(int r=0;r<4;r++){
-                for(int s=0;s<4;s++){
+                for(int s=0;s<4;s++)
+                {
                     p.setColor(Color.WHITE);
                     p.drawPolygon(tile[r][c][s]);
-                    //    System.out.print(board[r][c][s] + " ");
-                    if (board[r][c][s] != '-') {
-
+                    if (board[r][c][s] != '-')
+                    {
                         if(board[r][c][s]=='x')
                             p.setColor(new Color(255, 128, 128));
                         else if (board[r][c][s]=='o')
@@ -172,46 +144,38 @@ public class Tic_Tac_Toe_Display extends JPanel implements MouseListener, Runnab
                 }
             }
         }
-        //p1Wins, p2Wins, Ties
-        p.setFont(new Font("Sans Serif", Font.BOLD, 30));
-        p.setColor(new Color(255, 128, 128));
-        p.drawString("Player 1 wins: " + p1Wins/2, 400,100);
-        p.setColor(new Color(163, 185, 224));
-        p.drawString("Player 2 wins: " + p2Wins/2, 400,150);
 
-        //which player wins?
+        //displys p1Wins & p2Wins
+        if(!Ai1 || !Ai2)
+        {
+            p.setFont(new Font("Sans Serif", Font.BOLD, 30));
+            p.setColor(new Color(255, 128, 128));
+            p.drawString("Player 1 wins: " + p1Wins/2, 400,100);
+            p.setColor(new Color(163, 185, 224));
+            p.drawString("Player 2 wins: " + p2Wins/2, 400,150);
+        }
+        else{
+            p.setFont(new Font("Sans Serif", Font.BOLD, 30));
+            p.setColor(new Color(255, 128, 128));
+            p.drawString("Player 1 wins: " + p1Wins, 400, 100);
+            p.setColor(new Color(163, 185, 224));
+            p.drawString("Player 2 wins: " + p2Wins, 400, 150);
+        }
+
+        //prints which player wins
         p.setColor(new Color(141, 224, 148));
         if(isWinner(p1)){
             p.drawString("PLAYER 1 WINS" , 400,600);
             if(!replay && !Ai2 || !Ai1)
                 p.drawString("Press 'r' to replay" , 400,400);
-            p1Wins++;
         }
         else if (isWinner(p2)){
             p.drawString("PLAYER 2 WINS" , 400,600);
             if(!replay && !Ai2 || !Ai1)
                 p.drawString("Press 'r' to replay" , 400,400);
-            p2Wins++;
         }
 
     }
-public void displayBoard(){
-    for (int l = 0; l < 4; l++) {
-        for (int c = 0; c < 4; c++) {
-            for (int r = 0; r < 4; r++) {
-                System.out.println(board[r][c][l] +",");
-            }
-        }
-        System.out.println("\n");
-    }
-}
-
-
-    public void addNotify(){
-        super.addNotify();
-        requestFocus();
-    }
-
 
     @Override
     public void mouseReleased(MouseEvent e) {
@@ -225,7 +189,7 @@ public void displayBoard(){
                     if(!playerMoves(mouseX, mouseY, p1))
                         return;
 
-                } else if (!playerTurn) {
+                } else{
                     if(!playerMoves(mouseX, mouseY, p2))
                         return;
                 }
@@ -237,7 +201,6 @@ public void displayBoard(){
 
     @Override
     public void keyTyped(KeyEvent e) {
-
         if((isWinner(p1) || isWinner(p2)) && e.getKeyChar() == 'r'){
             System.out.println("r is pressed");
             reset();
@@ -245,8 +208,102 @@ public void displayBoard(){
         }
     }
 
-    public boolean playerMoves(double x, double y, char player){
+    @Override
+    public void run() {
+        System.out.println("running");
+        for (int switched = 0; switched < 2; switched++) {
+            //switches player
+            if (switched == 1) {
+                playerTurn = false;
+            }
 
+            for (i = 0; i < nGames; i++) {
+                while ((!isWinner(p1) && !isWinner(p2))) {
+                    //p1
+                    if (playerTurn) {
+                        //p1 is AI
+                        if (Ai1)
+                        {
+                            if(random) {
+                                Random_Ai random1 = new Random_Ai(p1, board, moved);
+                                random1.RandomAI_Move();
+                            }
+                            if(blocking)
+
+                            try {
+                                Thread.sleep(millisToSleep);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        //p1 is a human player
+                        else {
+                            playerInput = true;
+                            while (playerInput) {
+                                try {
+                                    Thread.sleep(50);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                        playerTurn = false;
+                        repaint();
+                    }
+                    //p2
+                    else {
+                        //p2 is RandomAi
+                        if (Ai2) {
+                            if(random) {
+                                Random_Ai random2 = new Random_Ai(p1, board, moved);
+                                random2.RandomAI_Move();
+                            }
+                            if(blocking)
+                            try {
+                                Thread.sleep(millisToSleep);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        //p2 is person
+                        else {
+                            playerInput = true;
+                            while (playerInput) {
+                                try {
+                                    Thread.sleep(50);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                        playerTurn = true;
+                        repaint();
+                    }
+                    try {
+                        Thread.sleep(GamePause);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (isWinner(p1))
+                    p1Wins++;
+                if (isWinner(p2))
+                    p2Wins++;
+                if (Ai1 && Ai2) {
+                    if (i != nGames)
+                        reset();
+                }
+            }
+            if (Ai1 && Ai2) {
+                if (i == nGames)
+                    System.out.print("game ends");
+                break;
+            }
+        }
+    }
+
+    public boolean playerMoves(double x, double y, char player)
+    {
         for(int c=0;c<4;c++){
             for(int r=0;r<4;r++){
                 for(int s=0;s<4;s++){
@@ -266,21 +323,7 @@ public void displayBoard(){
         }
         return false;
     }
-
-    public void RandomAImove(char player){
-        Location ran = new Location((int)(Math.random()*4), (int)(Math.random()*4), (int)(Math.random()*4));
-        moved = false;
-
-        do{
-            if(board[ran.getCol()][ran.getRow()][ran.getSheet()]=='-'){
-                board[ran.getCol()][ran.getRow()][ran.getSheet()] = player;
-                moved = true;
-            }
-            else
-                ran = new Location((int)(Math.random()*4), (int)(Math.random()*4), (int)(Math.random()*4));
-        }while(!moved);
-        displayBoard();
-    }
+    
   public void BlockingAi(char player){
         moved=false;
         while (moved==false) {
